4d45fa5917824e910a8f3b2f5e77009d35bce45c
pseudowalrus <pseudowalrus@protonmail.com>
2020-05-16 12:05:09 +0100

    Added random pilot name generator
    Method added to the Crew class that generates a random name of a gender based on the argument passed. This could be used when hiring new pilots.
    
    Human name format: Firstname + Initial.
    Robot name format: Alphabetical prefix + numerical suffix.
    
    Added robot serial number prefix length and suffix length attributes, so we can vary the format of the serial number if we wish to. Perhaps different kinds of robots have different kinds of serial number.
    
    Created buttons in the Crew panel for demo purposes.

---
+++ Assets/Scripts/Crew.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class Crew : MonoBehaviour
+{
+	private string[] maleNames = File.ReadAllLines("./Assets/ImportedAssets/male_names.txt");
+	private string[] femaleNames = File.ReadAllLines("./Assets/ImportedAssets/female_names.txt");
+
+	private string randomName;
+	private string firstName;
+	private char initial;
+	private int prefixLength = 3;
+	private int suffixLength = 4;
+
+	[SerializeField]
+	private Button randomMaleNameButton, randomFemaleNameButton, randomRobotNameButton;
+
+	[SerializeField]
+	private Text randomMaleNameText, randomFemaleNameText, randomRobotNameText;
+
+	public void Awake()
+	{
+		randomMaleNameButton.onClick.AddListener(delegate { randomPilotName("male"); });
+		randomFemaleNameButton.onClick.AddListener(delegate { randomPilotName("female"); });
+		randomRobotNameButton.onClick.AddListener(delegate { randomPilotName("robot"); });
+	}
+
+	private void randomPilotName(string gender)
+	{
+		if (gender == "male")
+		{
+			string firstName = maleNames[Random.Range(0, maleNames.Length)];
+			char initial = char.ToUpper((char)('a' + Random.Range(0, 26)));
+			randomName = $"{firstName} {initial}.";
+			randomMaleNameText.text = randomName;
+
+		}
+		else if (gender == "female")
+		{
+			string firstName = femaleNames[Random.Range(0, femaleNames.Length)];
+			char initial = char.ToUpper((char)('a' + Random.Range(0, 26)));
+			randomName = $"{firstName} {initial}.";
+			randomFemaleNameText.text = randomName;
+		}
+		// robot name 
+		else
+		{
+			string prefix = string.Empty; 
+			string suffix = string.Empty;
+
+			for (int i = 0; i < prefixLength; i++)
+			{
+				prefix += char.ToUpper((char)('a' + Random.Range(0, 26)));
+				Debug.Log("prefix " + suffix);
+			}
+			for (int i = 0; i < suffixLength; i++)
+			{
+				suffix += Random.Range(0, 9).ToString(); 
+				Debug.Log("suffix " + suffix);
+			}
+
+			randomName = $"{prefix}-{suffix}";
+			randomRobotNameText.text = randomName;
+		}
+	}
+}
