c9cfbfa7664355167f0540f05a55d54ba990fbf2
pseudowalrus <61809127+pseudowalrus@users.noreply.github.com>
2020-08-26 23:46:34 +0100

    Added support for customisation sprites via scriptableobjects. Left/right arrow btns made into prefabs. Refactored some UI code.

--- Assets/Scripts/CharacterCustomisation/CustomisationManager.cs
+++ Assets/Scripts/CharacterCustomisation/CustomisationManager.cs
@@ -7,11 +7,13 @@ using UnityEngine.UI;
 public class CustomisationManager : MonoBehaviour
 {
     public CustomisationTypeContainer customisationTypeContainer; 
-    public GameObject arrowButtonPrefab;
+    public GameObject leftArrowPrefab;
+    public GameObject rightArrowPrefab; 
     private GameObject customisationContainer;
-    private Color[] customisationColors = 
-    { 
-        Color.red, Color.green, Color.blue, Color.yellow, Color.white, Color.black, Color.cyan, Color.grey, Color.magenta
+
+    private Color[] colors = 
+    {
+        Color.red, Color.green, Color.blue, Color.yellow, Color.white, Color.black, Color.cyan, Color.grey
     };
 
     public enum Direction { Left = 0, Right = 1 }; 
@@ -19,38 +21,50 @@ public class CustomisationManager : MonoBehaviour
     void Start()
     {
         GenerateCustomisationOptions();
+        LogCustomisationState(); 
     }
 
     private void GenerateCustomisationOptions()
     {
-        // this is the overarching parent object 
-        customisationContainer = new GameObject("CustomisationContainer");
+        // This is the overarching parent object 
+        customisationContainer = new GameObject(CustomisationConstants.customisationContainerName);
         customisationContainer.transform.SetParent(gameObject.transform);
         customisationContainer.transform.localPosition = Vector3.zero;
+
+        RectTransform rectTransform = customisationContainer.AddComponent<RectTransform>();
+        rectTransform.anchorMin = new Vector2(0f, 0f);
+        rectTransform.anchorMax = new Vector2(1f, 1f);
+        rectTransform.localScale = Vector3.one;
+        rectTransform.offsetMin = Vector2.zero;
+        rectTransform.offsetMax = Vector2.zero;
+
         VerticalLayoutGroup verticalLayoutGroup = customisationContainer.AddComponent<VerticalLayoutGroup>();
-        verticalLayoutGroup.spacing = 16;
+        verticalLayoutGroup.spacing = CustomisationConstants.customisationContainerSpacing; 
 
         foreach (CustomisationType customisationType in customisationTypeContainer.customisationTypes)
         {
-            // this is the parent object for each individual row of the customisation container  
-            GameObject customisationOption = new GameObject("CustomisationOption");
+            // This is the parent object for each individual row of the customisation container  
+            GameObject customisationOption = new GameObject(CustomisationConstants.customisationOptionName);
             customisationOption.transform.SetParent(customisationContainer.transform);
             RectTransform customisationOptionRT = customisationOption.AddComponent<RectTransform>();
             customisationOption.AddComponent<HorizontalLayoutGroup>();
 
-            GameObject leftArrow = Instantiate(arrowButtonPrefab);
+            GameObject leftArrow = Instantiate(leftArrowPrefab);
             leftArrow.transform.SetParent(customisationOption.transform);
+            leftArrow.transform.localScale = new Vector2(0.25f, 0.25f); 
             Button leftArrowBtn = leftArrow.GetComponent<Button>();
-            leftArrowBtn.onClick.RemoveAllListeners();
+            leftArrowBtn.onClick.RemoveAllListeners(); 
 
-            GameObject imageObject = new GameObject("CustomisationImage");
+            GameObject imageObject = new GameObject(CustomisationConstants.customisationImageName);
             imageObject.transform.SetParent(customisationOption.transform);
-            RectTransform imageObjectRT = imageObject.GetComponent<RectTransform>();
-            customisationType.customisationImage = imageObject.AddComponent<Image>();
-            customisationType.customisationImage.color = customisationType.customisationColor;
+            imageObject.transform.localScale = Vector2.one;
+            customisationType.image = imageObject.AddComponent<Image>();
+            customisationType.image.sprite = customisationType.sprite; 
+            customisationType.image.color = customisationType.color;
 
-            GameObject rightArrow = Instantiate(arrowButtonPrefab);
+            GameObject rightArrow = Instantiate(rightArrowPrefab);
             rightArrow.transform.SetParent(customisationOption.transform);
+            rightArrow.transform.localScale = new Vector2(0.25f, 0.25f); 
             Button rightArrowBtn = rightArrow.GetComponent<Button>();
             rightArrowBtn.onClick.RemoveAllListeners();
 
@@ -63,28 +77,38 @@ public class CustomisationManager : MonoBehaviour
     {
         if (direction == Direction.Left)
         {
-            // this wraps around if the user reaches the end of the customisation options for that group 
-            if (customisationType.customisationOptionIndex > 0)
+            // This wraps around if the user reaches the end of the customisation options for that group 
+            if (customisationType.index > 0)
             {
-                customisationType.customisationOptionIndex--; 
+                customisationType.index--; 
             }
             else
             {
-                customisationType.customisationOptionIndex = customisationColors.Length - 1;  
+                customisationType.index = colors.Length - 1;  
             }
         }
         else if (direction == Direction.Right)
         {
-            if (customisationType.customisationOptionIndex < customisationColors.Length - 1)
+            if (customisationType.index < colors.Length - 1)
             {
-                customisationType.customisationOptionIndex++;
+                customisationType.index++;
             }
             else
             {
-                customisationType.customisationOptionIndex = 0; 
+                customisationType.index = 0; 
             }
         }
-        customisationType.customisationColor = customisationColors[customisationType.customisationOptionIndex];
-        customisationType.customisationImage.color = customisationType.customisationColor;
+        customisationType.color = colors[customisationType.index];
+        customisationType.image.color = customisationType.color;
+
+        LogCustomisationState(); 
+    }
+
+    void LogCustomisationState()
+    {
+        for (int i = 0; i < customisationTypeContainer.customisationTypes.Length; i++)
+        {
+            Debug.Log($"Customisation Type {i} value: {customisationTypeContainer.customisationTypes[i].index}"); 
+        }
     }
 }
--- Assets/Scripts/CharacterCustomisation/CustomisationType.cs
+++ Assets/Scripts/CharacterCustomisation/CustomisationType.cs
@@ -6,21 +6,8 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "CustomisationType", menuName = "ScriptableObjects/CustomisationType", order = 1)]
 public class CustomisationType : ScriptableObject
 {
-    public Image customisationImage;
-    //public GameObject leftArrow;
-    //public GameObject rightArrow;
-    public int customisationOptionIndex;
-    public Color customisationColor = Color.red; 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Image image;
+    public Sprite sprite; 
+    public int index;
+    public Color color; 
 }
